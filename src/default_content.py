a78fa35cae6099fc171e5265389b94dc7437da0a
David Harris <dharris3968@hotmail.co.uk>
2024-02-24 16:40:56 +0000

    Refactor layout first steps

---
+++ src/default_content.py
@@ -0,0 +1,91 @@
+'''
+Define, for this site, the default content of the page sections
+'''
+
+import dash_bootstrap_components as dbc
+from dash import page_container, dcc, html
+from src.lorem import lorem
+from src import utils
+
+def create_home_link(label):
+        return dcc.Link(
+            label,
+            href="/",
+            style={"color":"black", "textDecoration":"none"}
+        )
+# --------------------------------------------
+def create_header_left_column():
+    hl = html.Div(
+        [
+            html.H2(create_home_link("Dash Bootstrap Responsive Template"), className="wide-only vertical-center"),
+            html.H3(create_home_link("DBC Template"), className="narrow-only vertical-center"),
+        ],
+    )
+    return hl
+# --------------------------------------------
+def create_header_right_column():
+    hr = html.Div(
+            children=[
+                
+                dcc.Link(
+                    children=html.Img(
+                        src="/static/github-mark.svg", 
+                        height="40px", 
+                        style={"margin":"14px"}
+                    ),
+                    href="https://github.com/dh3968mlq/dash-bootstrap-starter-kit",
+                    target="_blank",
+
+                ),
+                html.Img(
+                    src="/static/radix-icons--hamburger-menu.svg", 
+                    height="40px", 
+                    style={"margin":"14px"},
+                    id="drawer-hamburger-button",
+                    className="narrow-only"
+                ),
+            ],
+            className="ms-auto"     # Right aligns the content
+        )
+    return hr
+# --------------------------------------------
+def create_header():
+    header = [
+            dbc.Stack(
+                children=[
+                    create_header_left_column(),
+                    create_header_right_column(),
+                ],
+                direction="horizontal",
+            ),
+        ]
+    return header
+# --------------------------------------------
+def create_left_sidebar(nav_data):
+    navbar = [
+            html.H2("Left sidebar"),
+            html.P("This sidebar is replaced with a pop-up drawer when screen width is below 1200px"),
+        ] + utils.create_side_nav_content(nav_data),
+    return navbar
+# --------------------------------------------
+def create_right_sidebar():
+    aside = [
+
+            html.H2("Right SideBar"),
+            html.P("This sidebar disappears when screen width is below 1500px")
+        ] + \
+        [html.H3("Long text to show scrolling")] + \
+        [html.P(lorem) for _ in range(6)]
+    return aside
+# --------------------------------------------
+def create_body():
+    body = html.H2("Page body placeholder"),
+    return body
+# -----------------------------------------------------------
+def create_footer():
+    footer = [
+        html.H4("Footer area",
+                      style={"color":"black", "textDecoration":"none"},
+                      className="vertical-center",)
+    ]
+    return footer
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from dash import html, dcc
+from dash import html, dcc, dbc
 from src.lorem import lorem
 
 def create_side_nav_content(nav_data):
@@ -24,3 +24,63 @@ def create_side_navbar_link(nav_entry):
             dcc.Link(nav_entry["name"], href=nav_entry["path"]),
         )
     return link
+# -------------------------------------------
+def create_layout_structure(header_height=70, header_contents=html.H2("Header"),
+        narrow_header_height=None, narrow_header_contents=None,
+        left_sidebar_contents=html.H4("Left sidebar"),
+        right_sidebar_contents=html.H4("Right sidebar"),
+        body_contents=html.H2("Page Body"),
+        footer_height=40, footer_contents=html.H4("Footer"),
+        narrow_footer_height=None, narrow_footer_contents=None,
+        fluid=True
+        ):
+    """
+        narrow_header_contents=None : header is unchanged on narrow screens. Set to [] for no contents
+    """
+    # Construct header (list)
+    if narrow_header_contents is None:
+        header = [html.Div(children=header_contents, className="page-header")]
+    else:
+        header = [
+            html.Div(children=header_contents, className="page-header wide-only"),
+            html.Div(children=narrow_header_contents, className="page-header narrow-only"),
+        ]
+
+    # Left sidebar (list) and the offcanvas that replaces it on a narrow screen
+        left_sidebar = [
+            html.Div(children=left_sidebar_contents, className="left-sidebar wide-only"),
+            dbc.Offcanvas(
+                id="components-navbar-drawer",
+                children=left_sidebar_contents,
+                className="left-sidebar narrow-only",
+                style={   # Styling .page-navbar in CSS doesn't seem to work to do this...
+                    "top":f"{header_height}px",
+                    "bottom":f"{footer_height}px",
+                }
+            )]
+
+    # Right sidebar (list) 
+    right_sidebar = [
+            html.Div(children=right_sidebar_contents, className="right-sidebar wide-only"),
+    ]
+
+    # Body (list)
+    body = html.Div(children=body_contents, className="page-body"),
+
+    # Footer (list) 
+    if footer_contents is None:
+        footer = []
+    elif narrow_footer_contents is None:
+        footer = [html.Div(children=footer_contents, className="page-footer")]
+    else:
+        footer = [
+            html.Div(children=footer_contents, className="page-footer wide-only"),
+            html.Div(children=narrow_footer_contents, className="page-footer narrow-only"),
+        ]
+
+    layout = dbc.Container(   
+        children= header + left_sidebar + right_sidebar + body + footer + \
+            [dcc.Location(id="main-url")],
+        fluid=fluid,
+    )
+    return layout
