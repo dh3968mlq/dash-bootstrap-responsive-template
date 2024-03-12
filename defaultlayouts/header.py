10084ef5a4aefcb51dac71f0cef34b5d8db5a81d
David Harris <dharris3968@hotmail.co.uk>
2024-03-12 08:05:38 +0000

    Tidying, update description

--- defaultlayouts/header.py
+++ defaultlayouts/header.py
@@ -21,49 +21,42 @@ def header_left_column():
     return hl
 # --------------------------------------------
 def header_right_column():
-    github_icon = dcc.Link(
-        html.I(className="bi bi-github fs-3", id="github-logo"),
-        href="https://github.com/dh3968mlq/dash-bootstrap-responsive-template",
-        target="_blank",
-    )
-    hr = html.Span(
-        children=[
-            iconbuttons.lightswitch(),
-            github_icon,
-        ],
-        className="ms-auto",     # Right aligns the content
-        style={"font-size":"10px"}, 
-    )
+    nav_data = page_registry.values()
+    hr = [
+        iconbuttons.lightswitch(),
+        dcc.Link(
+            html.I(className="bi bi-github fs-3", id="github-logo"),
+            href="https://github.com/dh3968mlq/dash-bootstrap-responsive-template",
+            target="_blank",
+        ),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem(nav_entry["name"], href=nav_entry["path"])
+                                    for nav_entry in nav_data
+                                        if nav_entry["path"][:7] != "/posts/" 
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Pages",
+            align_end=True,
+        ),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem(nav_entry["name"], href=nav_entry["path"])
+                                    for nav_entry in nav_data
+                                        if nav_entry["path"][:7] == "/posts/" 
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Posts",
+            align_end=True,
+        ),
+    ]
     return hr
 # --------------------------------------------
 def header():
-    nav_data = page_registry.values()
     header = dbc.NavbarSimple(
-        children=[
-            header_right_column(),
-            dbc.DropdownMenu(
-                children=[
-                    dbc.DropdownMenuItem(nav_entry["name"], href=nav_entry["path"])
-                                        for nav_entry in nav_data
-                                            if nav_entry["path"][:7] != "/posts/" 
-                ],
-                nav=True,
-                in_navbar=True,
-                label="Pages",
-                align_end=True,
-            ),
-            dbc.DropdownMenu(
-                children=[
-                    dbc.DropdownMenuItem(nav_entry["name"], href=nav_entry["path"])
-                                        for nav_entry in nav_data
-                                            if nav_entry["path"][:7] == "/posts/" 
-                ],
-                nav=True,
-                in_navbar=True,
-                label="Posts",
-                align_end=True,
-            ),
-        ],
+        children=header_right_column(),
         brand=header_left_column(),
         fluid=True,
         id="main-navbar"
--- posts/Template-Description.md
+++ posts/Template-Description.md
@@ -13,6 +13,7 @@ wraps the [Bootstrap](https://getbootstrap.com/docs/3.4/components/) components 
 ### This Template
 
 * Implements a basic responsive design
+    * Further suggestions and contributions are welcome - please feel free to raise an [issue](https://github.com/dh3968mlq/dash-bootstrap-responsive-template/issues)
 * Draws on the much more complex [code for the Dash Mantine Components Documentation](https://github.com/snehilvj/dmc-docs)
 * Is implemented almost entirely in Python using
     * [Dash](https://dash.plotly.com/urls)
@@ -22,6 +23,7 @@ wraps the [Bootstrap](https://getbootstrap.com/docs/3.4/components/) components 
     * The right sidebar disappears
     * The left sidebar is replaced by a pop-up drawer
     * The title becomes shorter and smaller
+    * The navigation in the header is replaced by a dropdown menu
 * Allows flexible control of the sidebar content
     * The left sidebar can share some, all or none of its content with the pop-up that replaces it on a small screen
     * Pages can have customised left or right sidebars and/or pop-ups
@@ -29,6 +31,10 @@ wraps the [Bootstrap](https://getbootstrap.com/docs/3.4/components/) components 
     * On narrow screens, controls can be moved to, or duplicated on, the pop-up sidebar 
 * Autogenerates content from Markdown files
 * Is configured for deployment on Heroku
+* Is not released as a package on PyPI
+    * Just download the code and make any changes you want
+* Has navigation both in the header and in the left sidebar (and two hamburger buttons on a small screen)
+    * It's expected one of these would be removed if it's used as the basis of a real application
 
 ### This Page
 
